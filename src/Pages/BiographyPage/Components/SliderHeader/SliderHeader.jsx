d478be8972a024afdb09c358406154af69b0672f
mohamedfariedm <mohamed.faried23267@gmail.com>
2025-01-01 09:32:14 +0200

    handel video play

--- src/Pages/BiographyPage/Components/SliderHeader/SliderHeader.jsx
+++ src/Pages/BiographyPage/Components/SliderHeader/SliderHeader.jsx
@@ -29,13 +29,7 @@ const SliderHeader = () => {
         <div className="banner-one section-padding bg-image h-[90vh] lg:h-[100vh]  ">
           <div className="video-bg overlay-bg animate-section opacity-0 transition-opacity duration-1000">
             <video
-              autoplay="autoplay"
-              className="video-src"
-              loop
-              muted
-              // @ts-ignore
-              playsInline="playsinline"
-              preload="auto"
+loop muted playsinline autoplay="autoplay"
             >
               <source src="https://s3-figma-videos-production-sig.figma.com/video/1252915703189651761/TEAM/bc1a/30a5/-ace5-4ced-8f63-278116711a10?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=OwZhfbxT4Gc4F3eOzNQcwTplKk5rzQOGY0ZqBibUF2AZwagwaWK9895xf656Rl6pvdjfAo37MyptFm2Rw3EbxzvS5fL7KFVFDTBwEmYGDhSIqU-5o8o8A80lJsbxbWzI~IrEr~y5B~X1jISd0zEMim3z-QB4Kchtj8yqGw-Eh5acCbLeRalDHazPjtTBhq-3-A37VHggqvx56upEH86duva3Tr02D8Dx8C6DSKPx~obaxcsCWf8N9qh4YDQVT5HXggkhdTzMvmoZ7-u4Viqxgwm~zo~OIFAClrLIkPWvWH7yXqiDlVPo4Ratb7cp63SW15yrJEgE9iIzQ2W5rlDIag__" type="video/mp4" />
             </video>
